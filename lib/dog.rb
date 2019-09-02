7e24739b89057b2e6f95c72bc1c0432e407c91dd
John Karrell Speck <speckworks@gmail.com>
2019-09-02 20:42:37 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  attr_accessor :dog_name, :dog_breed
+  @@all  = []
+  
+  def initialize(dog_name, dog_breed) 
+    name = @name
+    name= @name
+    @dog_breed = dog_breed
+    @@all<<self
+  end
+  
+end
+
+  
+  
+
+    
